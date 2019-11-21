895b0e7239f5741145c0af0a576cebc473be94e7
Yasmin Moraes da Silva <yasminms46@gmail.com>
2019-11-21 09:33:55 -0300

    Adicionando estrutura API

---
+++ notes-api/src/main/java/br/com/ifsul/notesapi/NotesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.notesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesApiApplication.class, args);
+    }
+
+}
---
+++ notes-api/src/test/java/br/com/ifsul/notesapi/NotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.notesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
